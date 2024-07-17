72cd15d3fbc10fef5799eedd99d44ca469a79fd5
twk00 <162117127+twk00@users.noreply.github.com>
2024-07-17 14:45:28 +0900

    Feature: server fix

--- app.js
+++ app.js
@@ -17,60 +17,126 @@ app.use(cors({
   methods: ["GET", "POST"],
   credentials: true
 }));
-let rooms = [];
+
+let rooms = new Map();
+
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.emit('ROOMS_UPDATE', rooms);
+  console.log('Client connected : ', socket.id);
+  const roomsObject = Object.fromEntries(rooms);
+  socket.emit('ROOMS_UPDATE', roomsObject); // 처음에 로비에서 게임룸 업데이트
 
   socket.on('ADD_ROOM', (roomData, callback) => {
+    // 게임룸 생성
     const roomId = uuidv4();
-    const newRoom = { ...roomData, roomId, participants: 0, status: '참가하기', owner: socket.id };
-    rooms.push(newRoom);
-    io.emit('ROOMS_UPDATE', rooms);
-    callback(newRoom);
+    const newRoom = { ...roomData, players: [], game: null, owner: socket.id };
+    rooms.set(roomId, newRoom);
+    
+    callback(roomId);
     socket.join(roomId);
     socket.roomId = roomId; // 소켓 객체에 roomId 저장
     socket.isOwner = true; // 소켓 객체에 소유자 표시
+    // Map을 객체로 변환하여 전송
+    const roomsObject = Object.fromEntries(rooms);
+    io.emit('ROOMS_UPDATE', roomsObject);
+
   });
 
   socket.on('join room', (roomId) => {
-    const room = rooms.find(r => r.roomId === roomId);
-    if (room && room.participants < 2) {
-      room.participants += 1;
-      if (room.participants === 2) {
-        room.status = '경기중';
-      }
-      io.emit('ROOMS_UPDATE', rooms);
-      socket.join(roomId);
+    // 게임룸 입장
+    const room = rooms.get(roomId);
+    const roomsObject = Object.fromEntries(rooms);
+    if (room && room.players.length < 2) {
+      room.players.push(socket.id);
+      socket.join(roomId)
       socket.roomId = roomId; // 소켓 객체에 roomId 저장
       socket.isOwner = socket.id === room.owner;
+
+      io.emit('ROOMS_UPDATE', roomsObject); // 로비 게임룸 정보 업데이트
+      if (room.players.length === 2){
+        const [player1, player2] = room.players;
+        room.game = new GameState(player1, player2);
+        io.to(roomId).emit('gameState', room.game.getGameState());
+      }
     }
   });
+
+  socket.on('ready', (data)=> {
+    // 준비상태관리
+    const roomId = socket.roomId;
+    const room = rooms.get(roomId);
+    if (room && room.game) {
+      const newState = room.game.setPlayerReady(socket.id, data.state);
+      console.log(newState)
+      io.to(roomId).emit('gameState', newState)
+    }
+  });
+
+  socket.on('attackDamage', (data) => {
+    // 데미지를 처리하는 로직
+    const roomId = socket.roomId;
+    const room = rooms.get(roomId);
+    if (room && room.game && room.game.gameStatus === 'playing') {
+      const targetPlayerId = room.players.find(id => id !== socket.id);
+      const newState = room.game.applyDamage(targetPlayerId, data.amount);
+      io.to(roomId).emit('gameState', newState)
+    }
+  })
+
+  socket.on('start', () => {
+    // 게임시작
+    const roomId = socket.roomId;
+    const room = rooms.get(roomId);
+    if (room && room.game) {
+      const newState = room.game.gameStart();
+      io.to(roomId).emit('gameState', newState)
+    }
+  })
+
+  // socket.on('castSkill', ({ skillType }) => {
+  //   const roomId = socket.roomId;
+  //   const room = rooms.get(roomId);
+  //   if (room && room.game && room.game.gameStatus === 'playing') {
+
+  //   }
+  // }) 
 
   socket.on('leave room', () => {
     const roomId = socket.roomId;
     if (roomId) {
       socket.leave(roomId);
-      const room = rooms.find(r => r.roomId === roomId);
+      const room = rooms.get(roomId);
       if (room) {
-        room.participants -= 1;
-        if (room.participants < 2) {
-          room.status = '참가하기';
+        room.players = room.players.filter(playerId => playerId !== socket.id);
+
+        if (socket.isOwner) {
+          const socketsInRoom = io.sockets.adapter.rooms.get(roomId);
+          if (socketsInRoom) {
+            socketsInRoom.forEach(socketId => {
+              const clientSocket = io.sockets.sockets.get(socketId);
+              if (clientSocket) {
+                clientSocket.leave(roomId);
+                clientSocket.emit('ROOM_CLOSE');
+              }
+            });
+          }
+          rooms.delete(roomId);
+        } else if (room.players.length === 0) {
+          rooms.delete(roomId);
         }
-        if (room.participants === 0 || socket.isOwner) {
-          rooms = rooms.filter(r => r.roomId !== roomId);
-        }
-        io.emit('ROOMS_UPDATE', rooms);
+
+        const roomsObject = Object.fromEntries(rooms);
+        io.emit('ROOMS_UPDATE', roomsObject);
         socket.to(roomId).emit('user_left', { userId: socket.id });
       }
     }
   });
 
+  // webRTC 연결관련
   socket.on('offer', (data) => {
     const { roomId, type, sdp } = data;
     socket.to(roomId).emit('offer', { offer: { type, sdp } });
   });
-
+  
   socket.on('answer', (data) => {
     const { roomId, type, sdp } = data;
     socket.to(roomId).emit('answer', { answer: { type, sdp } });
@@ -81,58 +147,73 @@ io.on('connection', (socket) => {
     socket.to(roomId).emit('candidate', { candidate });
   });
 
-  socket.on('ready', (roomId) => {
-    if (roomId) {
-      socket.to(roomId).emit('ready');
-    }
-  });
-
-  socket.on('cancelReady', (roomId) => {
-    if (roomId) {
-      socket.to(roomId).emit('cancelReady');
-    }
-  });
-
-  socket.on('damage', (data) => {
-    if (data.roomId) {
-      socket.to(data.roomId).emit('damage', { amount: data.amount });
-    }
-  });
-
-  socket.on('useSkill', ({ roomId, skillType }) => {
-    console.log('Connected : ', {roomId, skillType});
-    socket.to(roomId).emit('opponentSkillUsed', { skillType: skillType });
-  });
-
-  socket.on('ready', (data) => {
-    const { roomId, state } = data;
-    if (roomId) {
-      console.log('ready: ',roomId, state)
-      socket.to(roomId).emit('opponentIsReady', state);
-    }
-  });
-
-  // socket.on('disconnect', () => {
-  //   console.log('Client disconnected');
-  //   const roomId = socket.roomId;
-  //   if (roomId) {
-  //     const room = rooms.find(r => r.roomId === roomId);
-  //     if (room) {
-  //       if (room.participants === 1 && socket.isOwner) {
-  //         rooms = rooms.filter(r => r.roomId !== roomId);
-  //       } else {
-  //         room.participants -= 1;
-  //         if (room.participants < 2) {
-  //           room.status = '참가하기';
-  //         }
-  //       }
-  //       io.emit('ROOMS_UPDATE', rooms);
-  //       socket.to(roomId).emit('user_left', { userId: socket.id });
-  //     }
+  // socket.on('damage', (data) => {
+  //   if (data.roomId) {
+  //     console.log('damage: ', data.amount, data.socketId)
+  //     socket.to(data.roomId).emit('damage', { amount: data.amount, socketId: data.socketId });
   //   }
   // });
+
+  // socket.on('useSkill', ({ roomId, skillType }) => {
+  //   console.log('Connected : ', {roomId, skillType});
+  //   socket.to(roomId).emit('opponentSkillUsed', { skillType: skillType });
+  // });
+
+  // socket.on('ready', (data) => {
+  //   const { roomId, state } = data;
+  //   if (roomId) {
+  //     console.log('ready: ',roomId, state)
+  //     socket.to(roomId).emit('opponentIsReady', state);
+  //   }
+  // });
+
 });
 
 
 const PORT = process.env.PORT || 7777;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+class GameState {
+  constructor(player1Id, player2Id) {
+    this.players = {
+      [player1Id]: { health: 100, ready: false, skill: [null, null]},
+      [player2Id]: { health: 100, ready: false, skill: [null, null]}
+    };
+    this.gameStatus = 'waiting'; // 'waiting', 'bothReady' ,'playing', 'skillTime' ,'finished', 'replaying'
+    this.winner = null;
+  }
+
+  applyDamage(playerId, amount) {
+    this.players[playerId].health -= amount;
+    if (this.players[playerId].health <= 0) {
+      this.gameStatus = 'finished';
+      this.winner = Object.keys(this.players).find(id => id !== playerId);
+    }
+    return this.getGameState();
+  }
+
+  gameStart(){
+      this.gameStatus = 'playing';
+      this.winner = null;
+      return this.getGameState();
+  }
+
+  setPlayerReady(playerId, state) {
+    this.players[playerId].ready = state;
+    if (Object.values(this.players).every(player => player.ready)) {
+      this.gameStatus = 'bothReady';
+    } else {
+      this.gameStatus = 'waiting'
+    }
+    return this.getGameState();
+  }
+
+  getGameState() {
+    return {
+      players: this.players,
+      gameStatus: this.gameStatus,
+      winner: this.winner
+    };
+  }
+}
